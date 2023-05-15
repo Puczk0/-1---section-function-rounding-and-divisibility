faae4102db4d6c01e692bea0694cff09f17569ff
Puczk0 <74714430+Puczk0@users.noreply.github.com>
2023-05-15 17:04:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿int divisible = 0;
+int indivisible = 0;
+
+for (double i = 13; i < 24; i++)
+{
+    double y = (2 * i * 4 / 11);
+
+    if (Math.Round(y) % 11 == 0)
+    {
+
+        Console.WriteLine("For x equal to: " + i + " function value is: " + y.ToString("0.00"));
+        Console.WriteLine("Rounded: " + Math.Round(y));
+        Console.WriteLine("Is divisible by 11");
+        Console.WriteLine("----------");
+        divisible++;
+    }
+    else
+    {
+        Console.WriteLine("For x equal to: " + i + " function value is: " + y.ToString("0.00"));
+        Console.WriteLine("Rounded: " + Math.Round(y));
+        Console.WriteLine("Is not divisible by 11");
+        Console.WriteLine("----------");
+        indivisible++;
+    }
+}
+
+Console.WriteLine("Numbers divisible by 11: " + divisible);
+Console.WriteLine("Numbers not divisible by 11: " + indivisible);
